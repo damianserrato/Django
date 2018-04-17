5277cd174d491cc3aa0f0cf89283eb5e55b433e0
Damian Serrato <dami29@live.com>
2018-04-17 14:02:50 -0400

    First commit using Django

---
+++ blogs/apps/blogs_app/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+
+def index(request):
+    response = "Placeholder to later display all the list of blogs"
+    return HttpResponse(response)
+
+def new(request):
+    response = "Placeholder to display a new form to create a new blog"
+    return HttpResponse(response)
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    return HttpResponse("Placeholder to display blog " + number)
+
+def edit(request, number):
+    return HttpResponse("Placeholder to edit blog " + number)
+
+def destroy(request, number):
+    return redirect('/')
